f435e645492058d60031906d338867cde4da501f
jacktech555 <126461532+jacktech555@users.noreply.github.com>
2023-02-27 16:04:10 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -2,11 +2,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 //const config = require('config');
 //const mongoDbConnection = require('../config/connection');
-const controllers = require('./controllers/index');
+// const controllers = require('./controllers/index');
 
 const app = express();
+const router = express.Router();
+router.get('/ping',(req, res) => {
+  res.send("pong.");
+})
 app.use(bodyParser.json());
-app.use(controllers);
+// app.use(controllers);
 app.listen(3000, () => {
   //console.log(`listening to port ${config.get('port')}`);
 });
