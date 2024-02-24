bbf070c48357deb26e4cbc1d98c5f67577585645
jacktech555 <126461532+jacktech555@users.noreply.github.com>
2024-02-24 12:07:56 +0530

    Update index.js

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+router.get("/", (req, res) => {
+  res.send(`Your Email Is: umang`);
+});
 // const requireAuth = require('../middleware/requireAuth');
 
 // router.use('/user', require('./user'));
